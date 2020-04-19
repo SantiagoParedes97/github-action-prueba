df558e173965988db8d3604840525f4c12de7fb1
santiagoparedes97 <santiagoparedes97@github.com>
2020-04-19 19:24:31 -0300

    Primera implementacion XD

---
+++ googleSpreadsheet.js
@@ -0,0 +1,62 @@
+const {GoogleSpreadsheet} = require('google-spreadsheet')
+const core = require('@actions/core');
+
+//sacar esta villereada
+const tutors = [
+    {
+        coupleName:     'Rasta & Anto',
+        mails: ["santiagoparedes97@gmail.com"]
+    },
+    {coupleName:
+            'Guille & Maru'
+    },
+    {coupleName:
+            'Gise & Mariano'
+    },
+    {coupleName:
+            'Santi & DaniM'
+    },
+    {coupleName:
+            'Gaby & Tom'
+    },
+    {coupleName:
+            'Eze & NahueV'
+    },
+    {coupleName:
+            'Ivo & Ale'
+    },
+    {coupleName:
+            'Deby & Diana'
+    },
+    {coupleName:
+            'NicoR & MatiE'
+    },
+    {
+        coupleName:
+            'Alf & Facu'
+    }
+]
+async function getTutorsMailFor(githubUser) {
+    const doc = new GoogleSpreadsheet('1kavXnCLLdoKoXMVlw6_wFolzBvIuuNG9oqxNbc4Dwu8')
+    const client_email = core.getInput("google_api_client_email", {required: true})
+    const  private_key= core.getInput("private_key", {required: true})
+
+    await doc.useServiceAccountAuth({
+        client_email: client_email,
+        private_key: private_key,
+    });
+    await doc.loadInfo(); // loads document properties and worksheets
+    console.log(doc.title);
+
+    const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
+    const rows = await sheet.getRows({limit: 150,offset: 2}); // or use doc.sheetsById[id]
+    const getGithubUser = (student) =>  student._rawData[5];
+    const committerStudent = rows.find(student => getGithubUser(student) === githubUser)
+    const getTutors = (student) => student._rawData[7]
+
+    const getTutorsMail = (student) => tutors.find(tutorsCouple => tutorsCouple.coupleName === getTutors(student))
+
+    console.log(getTutorsMail(committerStudent));
+}
+
+module.exports = getTutorsMailFor
--- index.js
+++ index.js
@@ -1,16 +1,59 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const nodemailer = require("nodemailer")
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+const payload = github.context.payload;
+const commitName = payload.head_commit.message;
+const url = payload.repository.clone_url;
+
+const mailOptions = (from, to) => {
+    return {
+        from: from,
+        to: to,
+        subject: "La persona " + payload.head_commit.author.name + " termino el trabajo practico",
+        text: payload.repository.clone_url
+    };
 }
 
+const getTeachers = () => {
+    const getTeachersMails   = require('./googleSpreadsheet')
+    const teachersMails = getTeachersMails('AgustinBer');
+    console.log(teachersMails);
+    return teachersMails;
+}
+
+const sendMailToTeachers =async () => {
+    try {
+        const username = core.getInput("username", {required: true})
+        const password = core.getInput("password", {required: true})
+        const to = core.getInput("to", {required: true})
+        const from = core.getInput("from", {required: true})
+
+        const transport = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: username,
+                pass: password
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        });
+        const info = await transport.sendMail(mailOptions(from, username))
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+
+}
+
+async function main() {
+    await getTeachers()
+
+    if (commitName.includes("terminado")) {
+        sendMailToTeachers();
+    }
+}
+
+main()
