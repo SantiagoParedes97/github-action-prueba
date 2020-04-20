43389b914d8f4b97a5958b566bb8c84d5ddd7100
santiagoparedes97 <santiagoparedes97@github.com>
2020-04-20 14:28:14 -0300

    CRAP

--- src/index.js
+++ src/index.js
@@ -3,22 +3,24 @@ const bodyParser = require('body-parser');
 const http = require('http')
 const app = express();
 const server = http.Server(app);
+var GithubWebHook = require('express-github-webhook');
+var webhookHandler = GithubWebHook({ path: '/webhooks' });
 
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
+webhookHandler.on('*', function (event, repo, data) {
+    console.log("Evento: " + event)
+    console.log("repo: " + repo)
+    console.log("data: " + data)
+});
+app.use(webhookHandler)
 
 app.get('/', (req,res) => {
     res.send("OK")
 });
 
 
-app.post('/webhooks', (req,res) => {
-    console.log("request: ")
-    console.log(req)
-    res.send('ok')
-});
 var port=Number(process.env.PORT || 3000);
 server.listen(port, () => {
     console.log("servidor levantado")
