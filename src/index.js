edb00bba4fae52d1cda05944d4c9c2c4ca29f68e
santiagoparedes97 <santiagoparedes97@github.com>
2020-04-20 12:04:05 -0300

    Crap

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 
+app.get('/', (req,res) => {
+    res.send("OK")
+});
 
 
 app.post('/webhooks', (req,res) => {
