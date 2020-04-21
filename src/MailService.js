1cf91eb8b6876519e131f5968218e9e184773a9d
santiagoparedes97 <santiagoparedes97@github.com>
2020-04-21 00:19:36 -0300

    CRAP

--- src/MailService.js
+++ src/MailService.js
@@ -40,7 +40,7 @@ class MailService {
 
     sendMail(payload, config){
         const commitName = payload.head_commit.message;
-        console.log(payload)
+        console.log(payload.head_commit.author)
         if (commitName && commitName.toLowerCase().includes("terminado")) {
             return this.sendMailToTeachers(payload,config);
         }
